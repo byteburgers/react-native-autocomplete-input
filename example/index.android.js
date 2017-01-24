67b7609bdb487f40643929b9e9c6bf6352f906e1
Laurence Bortfeld <cheese@byteburgers.com>
2017-01-24 15:08:33 +0100

    Update dev dependencies

--- example/index.android.js
+++ example/index.android.js
@@ -3,6 +3,6 @@
  * https://github.com/l-urence/react-native-autocomplete-input
  */
 import { AppRegistry } from 'react-native';
-import AutocompleteExample from './Example.android.js';
+import AutocompleteExample from './Example';
 
 AppRegistry.registerComponent('AutocompleteExample', () => AutocompleteExample);
--- example/index.ios.js
+++ example/index.ios.js
@@ -3,6 +3,6 @@
  * https://github.com/l-urence/react-native-autocomplete-input
  */
 import { AppRegistry } from 'react-native';
-import AutocompleteExample from './Example.ios.js';
+import AutocompleteExample from './Example';
 
 AppRegistry.registerComponent('AutocompleteExample', () => AutocompleteExample);
