ad4fed2eda0a8fb0f643fc324a8d3ea01cf8983a
Zoltan Balogh <reddomester@gmail.com>
2020-03-10 23:16:15 +0200

    Update Autocomplete component to use hooks
    Also fixes a nuisance with the input styles being applied to the rendered TextInput

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import {
   FlatList,
@@ -10,149 +10,60 @@ import {
   ViewPropTypes as RNViewPropTypes
 } from 'react-native';
 
-// Keep this line for downwards compatibility with RN.
-// eslint-disable-next-line react/forbid-foreign-prop-types
 const ViewPropTypes = RNViewPropTypes || View.propTypes;
 
-class Autocomplete extends Component {
-  static propTypes = {
-    ...TextInput.propTypes,
-    /**
-     * These styles will be applied to the container which
-     * surrounds the autocomplete component.
-     */
-    containerStyle: ViewPropTypes.style,
-    /**
-     * Assign an array of data objects which should be
-     * rendered in respect to the entered text.
-     */
-    data: PropTypes.array,
-    /**
-     * Set to `true` to hide the suggestion list.
-     */
-    hideResults: PropTypes.bool,
-    /*
-     * These styles will be applied to the container which surrounds
-     * the textInput component.
-     */
-    inputContainerStyle: ViewPropTypes.style,
-    /*
-     * Set `keyboardShouldPersistTaps` to true if RN version is <= 0.39.
-     */
-    keyboardShouldPersistTaps: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.bool
-    ]),
-    /*
-     * These styles will be applied to the container which surrounds
-     * the result list.
-     */
-    listContainerStyle: ViewPropTypes.style,
-    /**
-     * These style will be applied to the result list.
-     */
-    listStyle: ViewPropTypes.style,
-    /**
-     * `onShowResults` will be called when list is going to
-     * show/hide results.
-     */
-    onShowResults: PropTypes.func,
-    /**
-     * method for intercepting swipe on ListView. Used for ScrollView support on Android
-     */
-    onStartShouldSetResponderCapture: PropTypes.func,
-    /**
-     * `renderItem` will be called to render the data objects
-     * which will be displayed in the result view below the
-     * text input.
-     */
-    renderItem: PropTypes.func,
-    keyExtractor: PropTypes.func,
-    /**
-     * `renderSeparator` will be called to render the list separators
-     * which will be displayed between the list elements in the result view
-     * below the text input.
-     */
-    renderSeparator: PropTypes.func,
-    /**
-     * renders custom TextInput. All props passed to this function.
-     */
-    renderTextInput: PropTypes.func,
-    flatListProps: PropTypes.object
-  };
+const Autocomplete = (props) => {
+  const {
+    containerStyle,
+    flatListProps,
+    hideResults,
+    inputContainerStyle,
+    keyboardShouldPersistTaps,
+    keyExtractor,
+    listContainerStyle,
+    listStyle,
+    onEndReached,
+    onEndReachedThreshold,
+    onShowResults,
+    onStartShouldSetResponderCapture,
+    renderItem,
+    renderSeparator,
+    renderTextInput: renderTextInputProp,
+  } = props;
 
-  static defaultProps = {
-    data: [],
-    keyboardShouldPersistTaps: 'always',
-    onStartShouldSetResponderCapture: () => false,
-    renderItem: ({ item }) => <Text>{item}</Text>,
-    renderSeparator: null,
-    renderTextInput: props => <TextInput {...props} />,
-    flatListProps: {}
-  };
+  const [data, setData] = useState(props.data);
+  const resultList = useRef(null);
+  const textInput = useRef(null);
 
-  constructor(props) {
-    super(props);
-    this.resultList = null;
-    this.textInput = null;
-
-    this.onRefListView = this.onRefListView.bind(this);
-    this.onRefTextInput = this.onRefTextInput.bind(this);
-    this.onEndEditing = this.onEndEditing.bind(this);
-  }
-
-  onEndEditing(e) {
-    this.props.onEndEditing && this.props.onEndEditing(e);
-  }
-
-  onRefListView(resultList) {
-    this.resultList = resultList;
-  }
-
-  onRefTextInput(textInput) {
-    this.textInput = textInput;
-  }
+  useEffect(() => {
+    setData(props.data);
+  }, [props.data]);
 
   /**
    * Proxy `blur()` to autocomplete's text input.
    */
-  blur() {
-    const { textInput } = this;
+  const blur = () => {
     textInput && textInput.blur();
-  }
+  };
 
   /**
    * Proxy `focus()` to autocomplete's text input.
    */
-  focus() {
-    const { textInput } = this;
+  const focus = () => {
     textInput && textInput.focus();
-  }
+  };
 
   /**
    * Proxy `isFocused()` to autocomplete's text input.
    */
-  isFocused() {
-    const { textInput } = this;
+  const isFocused = () => {
     return textInput && textInput.isFocused();
-  }
+  };
 
-  renderResultList() {
-    const {
-      data,
-      listStyle,
-      renderItem,
-      keyExtractor,
-      renderSeparator,
-      keyboardShouldPersistTaps,
-      flatListProps,
-      onEndReached,
-      onEndReachedThreshold
-    } = this.props;
-
+  const renderResultList = () => {
     return (
       <FlatList
-        ref={this.onRefListView}
+        ref={resultList}
         data={data}
         keyboardShouldPersistTaps={keyboardShouldPersistTaps}
         renderItem={renderItem}
@@ -164,52 +75,118 @@ class Autocomplete extends Component {
         {...flatListProps}
       />
     );
-  }
+  };
 
-  renderTextInput() {
-    const { renderTextInput, style } = this.props;
-    const props = {
-      style: [styles.input, style],
-      ref: this.onRefTextInput,
-      onEndEditing: this.onEndEditing,
-      ...this.props
+  const renderTextInput = () => {
+    const textInputProps = {
+      ref: textInput,
+      ...props
     };
 
-    return renderTextInput(props);
-  }
+    return renderTextInputProp(textInputProps);
+  };
 
-  render() {
-    const {
-      data,
-      containerStyle,
-      hideResults,
-      inputContainerStyle,
-      listContainerStyle,
-      onShowResults,
-      onStartShouldSetResponderCapture
-    } = this.props;
-    const showResults = data.length > 0;
+  const showResults = data.length > 0;
 
-    // Notify listener if the suggestion will be shown.
-    onShowResults && onShowResults(showResults);
+  // Notify listener if the suggestion will be shown.
+  onShowResults && onShowResults(showResults);
 
-    return (
-      <View style={[styles.container, containerStyle]}>
-        <View style={[styles.inputContainer, inputContainerStyle]}>
-          {this.renderTextInput()}
-        </View>
-        {!hideResults && (
-          <View
-            style={listContainerStyle}
-            onStartShouldSetResponderCapture={onStartShouldSetResponderCapture}
-          >
-            {showResults && this.renderResultList()}
-          </View>
-        )}
+  return (
+    <View style={[styles.container, containerStyle]}>
+      <View style={[styles.inputContainer, inputContainerStyle]}>
+        {renderTextInput()}
       </View>
-    );
-  }
-}
+      {!hideResults && (
+        <View
+          style={listContainerStyle}
+          onStartShouldSetResponderCapture={onStartShouldSetResponderCapture}
+        >
+          {showResults && renderResultList()}
+        </View>
+      )}
+    </View>
+  );
+};
+
+Autocomplete.propTypes = {
+  ...TextInput.propTypes,
+  /**
+   * These styles will be applied to the container which
+   * surrounds the autocomplete component.
+   */
+  containerStyle: ViewPropTypes.style,
+  /**
+   * Assign an array of data objects which should be
+   * rendered in respect to the entered text.
+   */
+  data: PropTypes.array,
+  /**
+   * Set to `true` to hide the suggestion list.
+   */
+  hideResults: PropTypes.bool,
+  /*
+   * These styles will be applied to the container which surrounds
+   * the textInput component.
+   */
+  inputContainerStyle: ViewPropTypes.style,
+  /*
+   * Set `keyboardShouldPersistTaps` to true if RN version is <= 0.39.
+   */
+  keyboardShouldPersistTaps: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.bool
+  ]),
+  /*
+   * These styles will be applied to the container which surrounds
+   * the result list.
+   */
+  listContainerStyle: ViewPropTypes.style,
+  /**
+   * These style will be applied to the result list.
+   */
+  listStyle: ViewPropTypes.style,
+  /**
+   * `onShowResults` will be called when list is going to
+   * show/hide results.
+   */
+  onShowResults: PropTypes.func,
+  /**
+   * method for intercepting swipe on ListView. Used for ScrollView support on Android
+   */
+  onStartShouldSetResponderCapture: PropTypes.func,
+  /**
+   * `renderItem` will be called to render the data objects
+   * which will be displayed in the result view below the
+   * text input.
+   */
+  renderItem: PropTypes.func,
+  keyExtractor: PropTypes.func,
+  /**
+   * `renderSeparator` will be called to render the list separators
+   * which will be displayed between the list elements in the result view
+   * below the text input.
+   */
+  renderSeparator: PropTypes.func,
+  /**
+   * renders custom TextInput. All props passed to this function.
+   */
+  renderTextInput: PropTypes.func,
+  flatListProps: PropTypes.object
+};
+
+Autocomplete.defaultProps = {
+  data: [],
+  defaultValue: '',
+  keyboardShouldPersistTaps: 'always',
+  onStartShouldSetResponderCapture: () => false,
+  renderItem: ({ item }) => <Text>{item}</Text>,
+  renderSeparator: null,
+  renderTextInput: props => {
+    const { style, ...rest } = props;
+    return <TextInput style={[styles.input, style]} {...rest} />
+  },
+  flatListProps: {}
+};
 
 const border = {
   borderColor: '#b9b9b9',
